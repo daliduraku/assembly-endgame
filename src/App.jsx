8a909e6889b6548e1e558c68e622b34c3c13e058
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-13 13:49:11 -0500

    feat: created two states for the current word and guessed letters

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { languages } from "./languages"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentWord, setCurrentWord] = useState("react")
+  const [guessedLetters, setGuessedLetters] = useState([])
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  function addGuessedLetter(letter) {
+    setGuessedLetters(prevLetters =>
+      prevLetters.includes(letter) ?
+        prevLetters : [...prevLetters, letter]
+    )
+  }
+
+  const languagesElements = languages.map(lang => {
+    const styles = {
+      backgroundColor : lang.backgroundColor,
+      color: lang.color
+    }
+    return(
+      <span
+        className='chip'
+        style={styles}
+        key={lang.name}
+      >
+        {lang.name}
+      </span>
+    )
+  })
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
