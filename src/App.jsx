383d2e5bf19387677ea19631d925d99838afb301
Dali Duraku <mesherbetmolla@gmail.com>
2025-02-14 12:46:43 -0500

    feat: conditionally rendering won/lost status
    - create a function for the logic to conditionally render the status of the game.

--- src/App.jsx
+++ src/App.jsx
@@ -76,6 +76,33 @@ function App() {
     )
   })
 
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost
+  })
+
+
+  function renderGameStatus(){
+    if(!isGameOver){
+      return null
+    }
+
+    if(isGameWon){
+      return(
+        <>
+          <h2>You win!</h2>
+          <p>Well done! 🎉</p>
+        </>
+      )
+    }else {
+      return(
+        <>
+          <h2>Game over!</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      )
+    }
+  }
 
   return (
     <main>
@@ -84,9 +111,8 @@ function App() {
         <p>Guess the word within 8 attempts to keep the
         programming world safe from Assembly!</p>
       </header>
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
+      <section className={gameStatusClass}>
+        {renderGameStatus()}
       </section>
       <section className="language-chips">
         {languageElements}
